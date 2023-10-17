eb4bd1733371db51ff27f62a8a066db2b9295dc0
spawnkid19xx <borisisac@gmail.com>
2023-10-17 21:41:59 +0100

    palindrome_number

---
+++ palindrome_number.py
@@ -0,0 +1,40 @@
+'''
+9.Palindrome Number
+Easy
+Topics
+Companies
+Hint
+
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+'''
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        for i in range(len(str(x))//2):
+            if str(x)[i] != str(x)[-((i+1))]:
+                print(str(x)[i], str(x)[-((i+1))], str(x)[i] != str(x)[-(i+1)])
+                return False
+        return True
+        
+a = Solution()
+a.isPalindrome(121) 
--- two_sum.py
+++ two_sum.py
@@ -35,7 +35,6 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i, a in enumerate(nums):
             for j in range(i + 1, len(nums)):
-               
                 if nums[i] + nums[j] == target:
                     return list[ i, j]
 a = Solution()
